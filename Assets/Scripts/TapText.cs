9bae16228ca58dc5e77014a348217b2d4d252552
Endah Ambar <ambarriris7@gmail.com>
2021-09-26 23:47:02 +0700

    Revisi Chapter 2

---
+++ Assets/Scripts/TapText.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TapText : MonoBehaviour
+{
+    public float SpawnTime = 0.5f;//spawn setiap 0.5 detik
+    public Text Text;
+
+    private float _spawnTime;
+
+    private void OnEnable()
+    {
+        _spawnTime = SpawnTime;
+    }
+
+
+
+    private void Update()
+    {
+        _spawnTime -= Time.unscaledDeltaTime;//nge spawn secara real time
+        if(_spawnTime<=0f)//jika tidak diklik maka spawn tidak berjalan
+        {
+            gameObject.SetActive(false);
+        }
+        else
+        {
+            Text.CrossFadeAlpha(0f, 0.5f, false);
+            if (Text.color.a == 0f)
+            {
+                gameObject.SetActive(false);
+            }
+        }
+    }
+}
